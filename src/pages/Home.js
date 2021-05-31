0dbb7439268a926ef023e1d3e6fddfa29ec9e606
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-05-31 23:32:33 +0200

    -

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import Footer from "../components/Footer"
 import Gradients from "../components/Gradients"
 import GradientsHeader from "../components/GradientsHeader"
+import Footer from "../components/Footer"
 import {FilterContextProvider} from "../context/FilterContext"
-import {GradientsContextProvider} from "../context/GradientsContext"
 
+import {GradientsContextProvider} from "../context/GratientsContext"
 const Home =()=>{
- return (
-        <>
+ return ( 
+      <div className="App min-vh-100 d-flex flex-column">
         <GradientsContextProvider>
           <GradientsHeader>
            <h1 className="display-1">Alyra Gradients</h1>
@@ -17,11 +17,9 @@ const Home =()=>{
             <FilterContextProvider>
               <Gradients />
            </FilterContextProvider>
-           
           </main>
-          </GradientsContextProvider>
-          <Footer/>
-        </>
-  )
+        </GradientsContextProvider>
+        <Footer />
+      </div>)
 }
 export default Home
