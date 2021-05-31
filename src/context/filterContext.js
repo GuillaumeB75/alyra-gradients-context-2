131a0191958f5e4a30e887e1140400673ab71f4a
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-06-01 00:28:13 +0200

    -

--- src/context/filterContext.js
+++ src/context/filterContext.js
@@ -1,20 +1,23 @@
-import {createContext, useContext, useState} from "react"
+import { createContext, useState, useContext } from "react"
 
-export const FilterContext = createContext()
-
-export const FilterContextProvider = ({children}) => {
-  const [filter, setFilter] = useState("all")
-  return (
-    <FilterContext.Provider value={{filter, setFilter}}>
-      {children}
-    </FilterContext.Provider>
-  )
-}
+// créer FilterContext object
+const FilterContext = createContext()
 
 export const useFilter = () => {
   const context = useContext(FilterContext)
   if (context === undefined) {
-    throw new Error(`You try to use FilterContext outside of its provider.`)
+    throw new Error(
+      `It seems that you are trying to use FilterContext outside of its provider`
+    )
   }
   return context
+}
+
+export const FilterContextProvider = ({ children }) => {
+  const [filter, setFilter] = useState("all")
+  return (
+    <FilterContext.Provider value={{ filter, setFilter }}>
+      {children}
+    </FilterContext.Provider>
+  )
 }
