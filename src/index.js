01dd95f465b4593329375cadf3d1f0b5deb1e64c
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-05-31 23:25:51 +0200

    -

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.css"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-import { BrowserRouter as Router } from "react-router-dom"
-import { GradientsContextProvider } from "./context/GradientsContext"
+import {BrowserRouter as Router} from "react-router-dom"
+import {GradientsContextProvider} from "./context/GradientsContext"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,29 +22,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
-
-
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import "bootstrap/dist/css/bootstrap.css"
-// import "./index.css"
-// import App from "./App"
-// import * as serviceWorker from "./serviceWorker"
-// import {BrowserRouter as Router} from "react-router-dom";
-// import {GradientsContextProvider} from "./context/GradientsContext"
-
-// ReactDOM.render(
-
-//     <Router>
-//       <GradientsContextProvider>
-//       <App />
-//       </GradientsContextProvider>
-//     </Router>
-//  ,
-//   document.getElementById("root")
-// )
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister()
